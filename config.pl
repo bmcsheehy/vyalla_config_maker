14813783efeab88f065c2d67a9adb2889fe915b4
Brian McSheehy <brian@Brians-MBP.attlocal.net>
2016-02-11 22:43:41 -0600

    adding vlan config with api calls

--- config.pl
+++ config.pl
@@ -2,25 +2,76 @@
 
 use warnings;
 use strict;
+use JSON qw( decode_json );
+use LWP::UserAgent;
+use open qw(:std :utf8);
+use Data::Dumper;
 
 my $conf_type;
 my $resp;
 my %resp_values;
 my @matches;
-my @config_content; 
+my @config_content;
 my $vlan_response = 'yes';
 my $result;
+
+my $sl_api_user = undef;
+my $sl_api_pass = undef;
+
+my $sl_vlan_endpoint = 'api.softlayer.com/rest/v3/SoftLayer_Account/getNetworkVlans.json';
+my $sl_subnet_endpoint = 'api.softlayer.com/rest/v3/SoftLayer_Network_Vlan/1098713/getSubnets.json';
+
+
+my @subnet_types = ('PRIMARY',
+                   'ADDITIONAL_PRIMARY',
+                   'SECONDARY',
+                   'ROUTED_TO_VLAN');
+
+sub get_vlans {
+    my $api_user = shift;
+    my $api_pass = shift;
+    my $ua = LWP::UserAgent->new( ssl_opts => { verify_hostname => 0 } );
+    my $sl_vlan_endpoint = 'api.softlayer.com/rest/v3/SoftLayer_Account/getNetworkVlans.json';
+    my $vlan_endpoint = "https://$api_user:$api_pass" . '@' . "$sl_vlan_endpoint";
+    my $response = $ua->get($vlan_endpoint);
+    if ( $response->is_success ) {
+        my $api_response = $response->decoded_content;
+        return $api_response;
+    }
+    else {
+        print Dumper $response;
+        die $response->status_line;
+    }
+}
+
+sub get_subnet_details {
+    my $api_user = shift;
+    my $api_pass = shift;
+    my $sl_vlan_id = shift;
+    my $ua = LWP::UserAgent->new( ssl_opts => { verify_hostname => 0 } );
+    my $sl_vlan_endpoint = "api.softlayer.com/rest/v3/SoftLayer_Network_Vlan/$sl_vlan_id/getSubnets.json";
+    my $vlan_endpoint = "https://$api_user:$api_pass" . '@' . "$sl_vlan_endpoint";
+    my $response = $ua->get($vlan_endpoint);
+    if ( $response->is_success ) {
+        my $api_response = $response->decoded_content;
+        return $api_response;
+    }
+    else {
+        print Dumper $response;
+        die $response->status_line;
+    }
+}
 
 sub parse_config {
     my $config_template = shift;
     my %resp_values = %{$_[0]};
     $conf_type = $_[1];
     @config_content = split "\n", $config_template;
-	print "\n## Auto Generated $conf_type Configuration ##\n\n";
+    print "\n## Auto Generated $conf_type Configuration ##\n\n";
     foreach my $line (@config_content) {
-	    @matches = ($line =~ /<(\S+)>/g);
+	  @matches = ($line =~ /<(\S+)>/g);
         for(@matches) {
-    	    my $replace = $resp_values{$_};
+    	      my $replace = $resp_values{$_};
             $line =~ s/<$_>/$replace/;
         }
         print "$line\n";
@@ -65,11 +116,12 @@ EOF
 print "\nxxxxx Vyatta Configuration Builder v.01 xxxxx\n\n";
 print "Choose from the following options:\n";
 print "1. IPSEC Configuration\n";
-print "2. VLAN Configuration\n";
+print "2. Manual VLAN Configuration\n";
+print "3. Automatic VLAN Configuration (API Access Required)\n";
 print "\nYour Choice: ";
 chomp(my $mode = <STDIN>);
-if( ($mode != 1) && ($mode != 2) ){
-	die("Invalid selection");
+if( ($mode < 1) && ($mode > 3) ) {
+    die("Invalid selection");
 }
 
 if($mode == 1) {
@@ -77,7 +129,7 @@ if($mode == 1) {
     $conf_type = 'IPSEC';
 
     print "\n\nxxxxx IPSEC Tunnel Configuration xxxxx\n\n";
- 
+
     # Capture Compression
     print "Compression [disable]: ";
     chomp($resp = <STDIN>);
@@ -106,7 +158,7 @@ if($mode == 1) {
     print "Preshared Secret Key: ";
     chomp($resp = <STDIN>);
     $resp_values{secret_key} = $resp;
- 
+
     # Peer Address
     print "Remote Peer Address: ";
     chomp($resp = <STDIN>);
@@ -130,11 +182,11 @@ if($mode == 1) {
     parse_config($ipsec_config_template, \%resp_values, $conf_type);
 }
 if($mode == 2) {
-    
+
     my $counter = 0;
 
     while($vlan_response =~ /^y/) {
-    
+
         $counter = $counter + 10;
 
         $conf_type = 'VLAN';
@@ -143,14 +195,14 @@ if($mode == 2) {
         print "\nVLAN Type [public]: ";
         chomp($resp = <STDIN>);
         if($resp ne "private") {
-    	    $resp = 'bond1'; 
+    	    $resp = 'bond1';
         }
         else {
     	    $resp = 'bond0'
         }
         $resp_values{vif_interface} = $resp;
-	
-	    # VLAN ID
+
+	      # VLAN ID
         print "VLAN ID: ";
         chomp($resp = <STDIN>);
         $resp_values{vif_id} = $resp;
@@ -159,14 +211,54 @@ if($mode == 2) {
         print "VLAN Address: ";
         chomp($resp = <STDIN>);
         $resp_values{vif_address} = $resp;
-   
+
         $resp_values{counter} = $counter;
         parse_config($vlan_config_template, \%resp_values, $conf_type);
 
         print "\nWould you like to add another VLAN [no]? ";
         chomp($vlan_response = <STDIN>);
 
-        
     }
+}
+if($mode == 3) {
+    $conf_type = 'VLAN';
 
+    print "Enter SoftLayer API Username: ";
+    chomp($sl_api_user = <STDIN>);
+
+    print "Enter SoftLayer API Password: ";
+    chomp($sl_api_pass = <STDIN>);
+
+    my $vlan_api_response = get_vlans($sl_api_user,$sl_api_pass);
+    my $vlan_json_decoded = decode_json($vlan_api_response);
+    my @api_vlans = @{ $vlan_json_decoded };
+    my $counter = 10;
+
+        foreach my $f (@api_vlans) {
+            #print $f->{"id"} . "\n";
+            my $subnet_api_response = get_subnet_details($sl_api_user,
+                                                         $sl_api_pass,
+                                                         $f->{"id"});
+            my $subnet_json_decoded = decode_json($subnet_api_response);
+            my @api_subnets = @{ $subnet_json_decoded };
+                foreach my $x (@api_subnets) {
+
+                    if($x->{"subnetType"} eq 'PRIMARY') {
+                        $resp_values{vif_id} = $f->{"id"};
+
+                        if($x->{"addressSpace"} eq 'PRIVATE') {
+                            $resp_values{vif_interface} = 'bond0';
+                        }
+                        else {
+                            $resp_values{vif_interface} = 'bond1';
+                        }
+                        $resp_values{vif_address} = $x->{networkIdentifier} .
+                                              '/' . $x->{"cidr"};
+
+                        $counter = $counter + 10;
+                        $resp_values{counter} = $counter;
+                        parse_config($vlan_config_template, \%resp_values, $conf_type);
+                    }
+                }
+        }
 }
